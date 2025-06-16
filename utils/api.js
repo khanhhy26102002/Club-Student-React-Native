1839648147e36b48867d5a715331b68d6ba28b54
khanhhy26102002 <ledangkhanhhy@gmail.com>
2025-06-16 14:33:00 +0700

    sửa theo api baseresponse

--- utils/api.js
+++ utils/api.js
@@ -5,4 +5,19 @@ const API = axios.create({
   timeout: 15000
 });
 console.log("API_URL", API_URL);
+export async function fetchBaseResponse(url, config) {
+  try {
+    const response = await API(url, config);
+    const { status, message, data } = response.data;
+
+    if (status >= 200 && status < 300) {
+      return data;
+    } else {
+      throw new Error(message || "Đã có lỗi xảy ra");
+    }
+  } catch (error) {
+    console.error("API ERROR:", error);
+    throw new Error(error.message || "Lỗi kết nối");
+  }
+}
 export default API;
