9f38145401c85ccbe9ccb1ac5e3e12825c8f4048
khanhhy26102002 <ledangkhanhhy@gmail.com>
2025-08-05 23:16:36 +0700

    sửa lại giao diện

--- Components/Students/Club/TabsFilter.jsx
+++ Components/Students/Club/TabsFilter.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity, Text, Animated } from "react-native";
 
 const tabs = [
   { key: "event", label: "Sự kiện" },
-  { key: "blog", label: "Blogs" },
+  { key: "blog", label: "Blogs" }
 ];
 
 export default function TabsFilter({ selected, onSelect }) {
@@ -12,25 +12,36 @@ export default function TabsFilter({ selected, onSelect }) {
       style={{
         flexDirection: "row",
         justifyContent: "space-around",
-        paddingVertical: 10
+        paddingVertical: 10,
+        borderRadius: 20,
+        marginHorizontal: 16,
+        marginTop: 4,
+        marginBottom: 8
       }}
     >
       {tabs.map((tab) => (
         <TouchableOpacity
           key={tab.key}
           onPress={() => onSelect(tab.key)}
+          activeOpacity={0.7} // tạo hiệu ứng mờ khi nhấn
           style={{
-            backgroundColor: selected === tab.key ? "#e7f3ff" : "transparent",
-            paddingVertical: 6,
-            paddingHorizontal: 14,
+            backgroundColor: selected === tab.key ? "#adadaeff" : "transparent",
+            paddingVertical: 8,
+            paddingHorizontal: 20,
             borderRadius: 20,
-            marginLeft: -55
+            shadowColor: selected === tab.key ? "#000" : "transparent",
+            shadowOpacity: selected === tab.key ? 0.08 : 0,
+            shadowRadius: 3,
+            width: 150
           }}
         >
           <Text
             style={{
               color: selected === tab.key ? "#1877f2" : "#555",
-              fontWeight: selected === tab.key ? "700" : "500"
+              fontWeight: selected === tab.key ? "700" : "500",
+              fontSize: 14,
+              textAlign: "center",
+              marginLeft: -8
             }}
           >
             {tab.label}
